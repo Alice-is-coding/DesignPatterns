0409a0eafbffba4644cf0c313a36dd77ce856ce8
Alice <alice.bord1@gmail.com>
2021-03-23 19:30:21 +0100

    Observer : Added a dedicated package [Observer], a Readme.txt where I group a number of explanations about what this pattern consists in, an [example0] package where I implemented a really simple case of this pattern : A bank account is observable by (1,n) persons. TODO : Coding the tests.

---
+++ src/BehavioralPatterns/Observer/example0/BankAccountOwner.java
@@ -0,0 +1,23 @@
+package BehavioralPatterns.Observer.example0;
+
+/**
+ * BankAccountOwner.
+ * ConcreteObserver.
+ *
+ * To watch a bank account.
+ *
+ * @author Alice B.
+ * @version 23/03/2021
+ */
+public class BankAccountOwner implements Observer{
+    /**
+     * To update the observer accordingly to the changes occurred in the Subject.
+     *
+     * @param observable The ConcreteSubject whose state changed.
+     * @param args Arguments following the changes (Changes themselves or others).
+     */
+    @Override
+    public void update(Subject observable, Object args) {
+        System.out.println(this + " updated by : " + observable + " - New balance : " + args);
+    }
+}
